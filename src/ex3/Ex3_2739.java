a589ead8d2e347f87e44b5e6aeb44e5656601029
희소 <87919596+kk1112k@users.noreply.github.com>
2021-10-27 22:11:00 +0900

    Add files via upload
    백준씨

---
+++ src/ex3/Ex3_2739.java
@@ -0,0 +1,18 @@
+package ex3_for;
+
+import java.util.Scanner;
+
+public class Ex3_2739 {
+
+	public static void main(String[] args) {
+		int N = new Scanner(System.in).nextInt();
+
+
+		for(int i = 1 ; i<10;i++)
+		{
+			System.out.println(N +"*"+ i +"="+ (N*1) );
+		}
+		
+	}
+
+}
