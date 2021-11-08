01519fea956adbb1e9653a26ac010a11e2612672
희소 <87919596+kk1112k@users.noreply.github.com>
2021-11-08 18:33:33 +0900

    Add files via upload

---
+++ src/ex4/EX_1110.java
@@ -0,0 +1,27 @@
+package ex4_while;
+
+import java.util.Scanner;
+
+public class EX_1110 {
+public static void main(String args[]){
+
+		Scanner in = new Scanner(System.in);
+ 
+		int N = in.nextInt();
+		in.close();
+        
+		int cnt = 0;
+		int copy = N;
+        
+		while (true) {
+			N = ((N % 10) * 10) + (((N / 10) + (N % 10)) % 10);
+			cnt++;
+ 
+			if (copy == N) {
+				break;
+			}
+		}
+		System.out.println(cnt);
+	}
+}
+
